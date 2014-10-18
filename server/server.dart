8a74f2820434c603d11ebb154b4c8ec98a108205
Matthew Butler <butler.matthew@gmail.com>
2014-10-18 16:03:14 -0300

    Remove unused route

--- server/server.dart
+++ server/server.dart
@@ -6,16 +6,5 @@ import 'package:mongo_dart/mongo_dart.dart';
 
 part 'lib/signin.dart';
 
-@Route("/hello")
-class HelloWorld extends Vane {
-  var pipeline = [Log, This];
-
-  @Route("")
-  @Route("/{message}")
-  Future hello(String message) {
-    return close("Hello ${message != "" ? message : "World"}!");
-  }
-}
-
 void main() => serve();
 
