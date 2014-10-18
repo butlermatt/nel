d391268ef7b4393cbc73a916186ea63c7cdb781f
Matthew Butler <butler.matthew@gmail.com>
2014-10-18 14:29:49 -0300

    Start server signup stuff

--- server/lib/signin.dart
+++ server/lib/signin.dart
@@ -6,10 +6,22 @@ class NelSignin extends Vane {
 
   @Route('', method: 'POST')
   Future signIn() {
-    log.info('Received Data: $json');
-    var data = new Map<String, String>();
-    data['success'] = 'Yes!';
-    return close(data);
+    log.info('Login request for: ${json['name']}');
+
+    mongodb.then((db) {
+      var usrCol = db.collection('users');
+
+      usrCol.findOne(where.eq('name', json['name'])
+                    .and(where.eq('password', json['password'])))
+        .then((nelUser) {
+        if(nelUser == null) {
+          return close({'error' : 'Invalid username or password'});
+        }
+      });
+
+    });
+
+    return end;
   }
 
   @Route('', method: 'GET')
@@ -17,5 +29,31 @@ class NelSignin extends Vane {
     log.warning('Somone tried to browse to signin link');
     write('That was bad!');
     return close('That was bad!');
+  }
+
+  @Route('/new', method: 'POST')
+  Future signUp() {
+    log.info('Received signUp request for ${json['name']}');
+
+    mongodb.then((db) {
+      var usrCol = db.collection('users');
+
+      usrCol.findOne(where.eq('name', json['name'])
+                    .or(where.eq('email', json['email'])))
+        .then((res) {
+
+        if(res != null) {
+          var error =  {};
+          if(res['name'] == json['name']) {
+            error['error'] = 'A user already exists with that name';
+          } else {
+            error['error'] = 'A user has already signed up with that email account';
+          }
+          return close(error);
+        }
+      });
+    });
+
+    return end;
   }
 }
--- server/server.dart
+++ server/server.dart
@@ -2,6 +2,7 @@ library nel.server;
 
 import 'dart:async';
 import 'package:vane/vane.dart';
+import 'package:mongo_dart/mongo_dart.dart';
 
 part 'lib/signin.dart';
 
