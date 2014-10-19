ee1f67d3ffbc39ecaf91c2e974c219f22d20710e
Matthew Butler <butler.matthew@gmail.com>
2014-10-19 13:44:52 -0300

    Add item provider to make managing easier

---
+++ client/lib/src/item_provider.dart
@@ -0,0 +1,39 @@
+library nel.item.provider;
+
+import 'nel_object.dart';
+
+class ItemProvider {
+  static ItemProvider _cache;
+
+  Map<String, NelNode> items;
+  NelNode _root;
+
+  factory ItemProvider() {
+    if(_cache == null) {
+      _cache = new ItemProvider._internal();
+    }
+    return _cache;
+  }
+
+  ItemProvider._internal() {
+    items = new Map<String, NelNode>();
+  }
+
+  void set root(NelNode val) {
+    _root = val;
+    items[val.objId] = val;
+  }
+
+  NelNode get root => _root;
+
+  NelNode getNodeById(String id) {
+    var node = items[id];
+    // if null, query server?
+    return node;
+  }
+
+  void add(NelNode node) {
+    items[node.objId] = node;
+  }
+
+}
